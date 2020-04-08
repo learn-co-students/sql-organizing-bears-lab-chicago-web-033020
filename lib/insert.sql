91e7a4765026f721cbf87761347331ac703907f1
imperfectcastle <bart.sakowski@gmail.com>
2020-04-08 16:36:05 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 2, 'M', 'Brown', 'Happy', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, 'Rowdy', 4, 'F', 'Brown', 'Crazy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, 'Tabitha', 3, 'F', 'Black', 'Happy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 'Sergeant Brown', 3, 'M', 'Brown', 'Crazy', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, 'Melissa', 7, 'F', 'White', 'Happy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, 'Grinch', 3, 'M', 'Green', 'Sad', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, 'Wendy', 1, 'F', 'Brown', 'Happy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, NULL, 'M', 'Black', 'Crazy', 0);
